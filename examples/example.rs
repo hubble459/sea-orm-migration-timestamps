1985fffc4acfce4bc0f9e8345709d81e629bc253
meep334 <geraldd459@gmail.com>
2023-04-25 15:02:41 +0200

    fix(import): fixed bad import in example and test

--- examples/example.rs
+++ examples/example.rs
@@ -1,5 +1,5 @@
 use sea_orm_migration::prelude::*;
-use sea_orm_migration_timestamps::TimestampExt;
+use sea_orm_migration_timestamps::CreateTableExt;
 
 #[derive(Iden)]
 enum ExampleTable {
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests {
     use sea_orm_migration::prelude::*;
-    use sea_orm_migration_timestamps::TimestampExt;
+    use sea_orm_migration_timestamps::CreateTableExt;
 
     #[derive(Iden)]
     enum ExampleTable {
